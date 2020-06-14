6daf904af281808c14b6187178434f4d8ea91850
AlejandroPonce077 <poncealejandro07@gmail.com>
2020-06-14 02:18:06 -0500

    Comentarios de programa

--- agrupamientoAAA.py
+++ agrupamientoAAA.py
@@ -14,16 +14,18 @@ from scipy.cluster.hierarchy import dendrogram
 
 dataset = pd.read_excel('C:/Users/Ponce/Desktop/Capacidades.xlsx')
 X = dataset.iloc[0:, 1:].values
+#obteniendo dendrograma
 dendrogram = sch.dendrogram(sch.linkage(X, method='ward'))
 model = AgglomerativeClustering(n_clusters=5, affinity='euclidean', linkage='ward')
 #hc = model
 model.fit(X)
 labels = model.labels_
-
+#graficando los clusters
 plt.scatter(X[labels==0, 0], X[labels==0, 1], s=50, marker='o', color='red')
 plt.scatter(X[labels==1, 0], X[labels==1, 1], s=50, marker='o', color='blue')
 plt.scatter(X[labels==2, 0], X[labels==2, 1], s=50, marker='o', color='green')
 plt.scatter(X[labels==3, 0], X[labels==3, 1], s=50, marker='o', color='purple')
 plt.scatter(X[labels==4, 0], X[labels==4, 1], s=50, marker='o', color='orange')
+#ver que datos pertenecen a los cluster obtenidos.
 #y_hc = hc.fit_predict(X)
 plt.show()
